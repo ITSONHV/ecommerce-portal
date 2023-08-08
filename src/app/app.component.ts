78360d999cb7b34724c2b7f457b8cc1170ced724
tuandt2 <tuandt2@sendo.vn>
2023-08-08 17:17:09 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -76,7 +76,7 @@ export class AppComponent implements OnInit  {
     this.initFacebookService();
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
-         this.showHeader = this.activatedRoute?.firstChild?.snapshot.data?.['showHeader'] !== false ?? false;
+         this.showHeader = this.activatedRoute?.firstChild?.snapshot.data?.['showHeader'] ?? true;
         // this.showSidebar = this.activatedRoute.firstChild.snapshot.data.showSidebar !== false;
         // this.showFooter = this.activatedRoute.firstChild.snapshot.data.showFooter !== false;
       }
