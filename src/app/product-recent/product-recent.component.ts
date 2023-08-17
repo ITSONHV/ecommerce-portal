3806a49eb29642b9fdb80d99c4a250ce3ac11b6a
Hoàng Văn Sơn <hoangvansonitech@gmail.com>
2023-08-17 22:31:02 +0700

    update code client

--- src/app/product-recent/product-recent.component.ts
+++ src/app/product-recent/product-recent.component.ts
@@ -7,6 +7,9 @@ import { ProductModel } from 'src/models/product.model';
 import { MainService } from 'src/services/main.service';
 import { SwalService, TYPE } from 'src/services/swal.service';
 import { SlickCarouselComponent } from "ngx-slick-carousel";
+import { ObjectModel } from 'src/models/object_paging.model';
+import { EncryptService } from 'src/services/encrypt.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 @Component({
   selector: 'app-product-recent',
   templateUrl: './product-recent.component.html',
@@ -15,10 +18,41 @@ import { SlickCarouselComponent } from "ngx-slick-carousel";
 export class ProductRecentComponent implements OnInit, AfterViewInit {
   @ViewChild("slickModal") slickModal: SlickCarouselComponent;
   @Input() listProduct: any;
+  public checkLoadData = false;
   isShowQuickView = false;
   @Input() isAutoPlay = false;
+  public product : any;
+  public imgfirst : string;
+  public quantity = 1;
   isMobile = true;
   public productSlugToChild: string;
+  slideConfigQuickView = {
+    "slidesToShow": 3,
+    "slidesToScroll": 3,
+   
+    "autoplay": true,
+    "autoplaySpeed": 4000,
+    "infinity": true,
+    "centerMode": true,
+    "pauseOnFocus": true,
+    "pauseOnHover": false,
+    "swipeToSlide": false,
+    "variableWidth": false,
+    "centerPadding": "0px",
+    "arrows": true,
+    "responsive": [
+      {
+        "breakpoint": 1024,
+        "settings": {
+          "arrows": false,
+          "centerMode": false,
+          "slidesToShow": 3,
+           "centerPadding": "0px",
+        },
+      },
+     
+    ],
+  };
   slideConfig = {
     "slidesToShow": 4,
     "slidesToScroll": 2,
@@ -75,7 +109,9 @@ export class ProductRecentComponent implements OnInit, AfterViewInit {
   constructor(private _svc: MainService, private _router: ActivatedRoute,
     @Inject(DOCUMENT) private document: Document,
     private router: Router,
-    private _swal: SwalService, )
+    private _swal: SwalService, 
+    private _encryptSvc: EncryptService, private spinner: NgxSpinnerService,
+    )
      {
     this.getScreenSize();
   }
@@ -118,7 +154,35 @@ export class ProductRecentComponent implements OnInit, AfterViewInit {
       this.productRecents = this._svc.getProductRecent();
     }
   }
-
+  getProductbyProductNameSlug(slug : string){
+    this.spinner.show();
+    var request = this._encryptSvc.encrypt(JSON.stringify({slug : slug})) 
+    this._svc.getProductbyProductNameSlug(request).subscribe(
+      (respones: ObjectModel)=>{
+        this.product = respones.data;
+        debugger
+        if(this.product != null){
+          // this.meta.updateTag({ name: 'description', content: this.product.seoDescription ?? ""});
+          // this.titleService.setTitle(this.product.seoTitle ?? "");
+          // this.meta.updateTag({ name: 'keywords', content: this.product.seoKeyword ?? ""});
+          
+          this.imgfirst = this.product.productImages[0]?.imageUrl??"";
+          //this.product.productImages?.shift();
+          //console.log(this.product);
+          // this.htmlContent = this.product.content;
+          // this.htmlDescription = this.product.description;
+          // this.getReviewProducts(this.product.id);
+          // this.getProductsRelate(this.product.categoryId);
+        }
+        this.checkLoadData = true;
+        this.spinner.hide();
+      },
+      (err) =>{
+        console.log(err);
+        this.spinner.hide();
+      }
+    );
+  }
   handleViewDetailProduct(event: any, product: any): void {
     const queryParams: Params = { slug: product.productNameSlug };
     this.router.navigate(
@@ -156,5 +220,48 @@ export class ProductRecentComponent implements OnInit, AfterViewInit {
   }
   showAddCartFavorite() {
     this._swal.toast(TYPE.SUCCESS, "Sản phẩm đã được thêm vào yêu thích.", false);
+  }
+  changeImageLarge(idImage: number){
+    let imageClick : any;
+    this.product.productImages.filter ((item: any) =>{
+       if(item.id === idImage){
+        imageClick =item;
+        return;
+       }
+    });
+    if(imageClick && imageClick !== undefined && imageClick != null){
+      this.imgfirst = imageClick.imageUrl;
+    }
+    
+  }decreaseQuantity(): void{
+    if (!isNaN(this.quantity) && this.quantity > 0) {
+      this.quantity--;
+    }
+    else if(!isNaN(this.quantity) && this.quantity === 0){
+      return;
+    }
+    else{
+      this._swal.Swal(
+        'Số lượng không hợp lệ!',
+        'Thông báo',
+        'warning',
+        'Đóng',
+        document.getElementById('#quick_view_popup-overlay')
+      )
+    }
+  }
+  increaseQuantity(): void{
+    if (!isNaN(this.quantity)) {
+      this.quantity++;
+    }
+    else{
+      this._swal.Swal(
+        'Số lượng không hợp lệ!',
+        'Thông báo',
+        'warning',
+        'Đóng',
+        document.getElementById('#quick_view_popup-overlay')
+      )
+    }
   }
 }
